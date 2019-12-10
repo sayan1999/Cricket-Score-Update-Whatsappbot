21e91bc05744ed8d8d9f5154bf3bc37421b5cd77
sayan1999 <mr.sayan.dey@gmail.com>
2019-12-10 15:27:43 +0530

    LICENSE

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import cricapi.cricapi as cricapi
 from pathlib import Path
 import twilio_help.twilio_help as twilio_help
 
+home = str(Path.home())
 
-s=cricapi.CricAPI("./config.json")
+s=cricapi.CricAPI(home + '/.cricapi/config.json')
 s.get_unique_id_and_score()
 
 message = ""
@@ -15,7 +16,7 @@ else:
 	for s in s.score:
 		message += s + "\n\n"
 
-home = str(Path.home())
+
 cli = twilio_help.Twilio_Client(home + '/.twilio/secret.json')
 cli.send_message(message)
 
